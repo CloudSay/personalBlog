d18bfc1287c458ce7c00b50aecb57689bc3b9743
yunfei.huang <yunfei.huang@aliyun.com>
2020-07-27 10:54:53 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
 module.exports = {
   title: 'cloudSay',
   description: 'personal blog',
-  base:'/personalBlog/'
+  base:'/personalBlog/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '小程序', link: '/mp/mp-ch' },
+      { text: 'GIT', link: '/git/git-zh' },
+      { text: 'js', link: '/js/js-ch' },
+    ],
+    sidebar: 'auto'
+  }
 }
